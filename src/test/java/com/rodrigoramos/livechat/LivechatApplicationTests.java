4f78605dd8c1e9da1e50c5a3361946cdd34bd749
Rodrigo Ramos <digojramos@gmail.com>
2024-10-18 08:53:03 -0300

    Project created

---
+++ src/test/java/com/rodrigoramos/livechat/LivechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoramos.livechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
